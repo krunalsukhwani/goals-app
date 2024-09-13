404fc665c554d9793777c648eaf0b93c9040d1e0
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-12 21:45:17 -0400

    Working with components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
+import GoalList from "./Components/GoalList/GoalList";
 
 const App = () => {
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <ul className="goal-list">
-        <li>Create React Application</li>
-        <li>Learn full stack tools and technologies</li>
-        <li>Complete Assignment, Quizes and Tests</li>
-      </ul>
+      <GoalList />
     </div>
   );
 };
