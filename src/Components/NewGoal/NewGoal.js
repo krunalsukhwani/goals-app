3000ffa02a95fce94c74098e897b85f36f875396
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-19 19:46:14 -0400

    Two way binding

--- src/Components/NewGoal/NewGoal.js
+++ src/Components/NewGoal/NewGoal.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewGoal.css";
 
 const NewGoal = props => {
+
+    const [enteredText, setEnteredText] = useState('');
 
     const addGoalHandler = event => {
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'My New Goal!'
+            text: enteredText
         };
 
+        //set blank value into the new state (setEnteredText)
+        setEnteredText('');
+    
         // added new props name as "onAddGoal" and we passed newGoal into this new props
         props.onAddGoal(newGoal);
     };
 
+    const textChangeHandler = event => {
+        //it will take value from text box and store into the variable 'enteredText'
+        setEnteredText(event.target.value);
+    };
+
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+    {/* change the text, call onChange event */}
+      <input type="text" value={(enteredText)} onChange={textChangeHandler}/>
       <button type="submit">Add Goal</button>
     </form>
   );
