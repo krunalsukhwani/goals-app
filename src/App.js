9381c72fbad77d98beaab2d63beb9ae12a080aae
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-19 19:02:49 -0400

    Created New Component
    Added NewGoal Component in the App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./App.css";
 import GoalList from "./Components/GoalList/GoalList";
+import NewGoal from "./Components/NewGoal/NewGoal";
 
 const App = () => {
-
   const courseGoals = [
-    {id:"cg1", text:"Create React Application"},
-    {id:"cg2", text:"Learn full stack tools and technologies"},
-    {id:"cg3", text:"Complete Assignment, Quizes and Tests"},
-    {id:"cg4", text:"Download code from github"},
+    { id: "cg1", text: "Create React Application" },
+    { id: "cg2", text: "Learn full stack tools and technologies" },
+    { id: "cg3", text: "Complete Assignment, Quizes and Tests" },
+    { id: "cg4", text: "Download code from github" },
   ];
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
+      <NewGoal />
       {/* props: pass courseGoals array to GoalList component 
         props holds the reference of an array or reference of a method
       */}
-      <GoalList goals={courseGoals}/>
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
